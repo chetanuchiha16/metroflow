0dcdbaee298bb3f5ac518e0a535200ef12710f46
chetanuchiha16 <chetan16ck@gmail.com>
2026-06-16 16:46:50 +0530

    Refactor server implementation: move TCP server logic to internal server package

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,2 +1,55 @@
 package server
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+type server struct {
+	ln net.Listener
+}
+
+func NewServer() server {
+	return server{}
+}
+
+func (sv *server) StartServer() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sv.ln = ln
+	fmt.Printf("server started at %v\n", ln.Addr().String())
+	sv.AcceptLoop()
+}
+
+func (sv *server) AcceptLoop() {
+	for {
+		conn, err := sv.ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("client connected at: %v\n", conn.RemoteAddr().String())
+		buffer := fmt.Appendf(nil, "connected to %v\n", conn.LocalAddr().String())
+		conn.Write(buffer)
+		go sv.HandleConn(conn)
+	}
+}
+
+func (sv *server) HandleConn(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	sv.ReadLoop(conn, buffer)
+}
+
+func (*server) ReadLoop(conn net.Conn, buffer []byte) {
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Printf("client %v disconnected %v\n", conn.RemoteAddr().String(), err)
+			return
+		}
+		fmt.Println(string(buffer[:n]))
+	}
+}
