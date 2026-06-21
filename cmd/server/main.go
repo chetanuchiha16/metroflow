6177a2ae64c9816c1830ac27e76f5fb809ed76cf
chetanuchiha16 <chetan16ck@gmail.com>
2026-06-21 11:19:06 +0530

    Refactor server initialization to handle exit channel and improve connection handling with timing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"metroflow/internal/server"
+	"os"
+
+	// "os"
+	"time"
 )
 
 func main() {
-	server := server.NewServer()
+	start := time.Now()
+	exit := make(chan bool)
+	go func() { // wait for exit before starting, else this go routine hasnt even started
+		if (<-exit) {
+			// os.Exit(0)
+			fmt.Printf("all jobs finished in %v\n", time.Since(start)) // does this really mean all jobs ? or the handle connection one ?
+			os.Exit(0)
+		}
+	}()
+	server := server.NewServer(exit)
 	server.StartServer()
+	
+
 }
 
 
--- internal/server/server.go
+++ internal/server/server.go
@@ -6,15 +6,18 @@ import (
 	"log"
 	"net"
 	"sync"
+
+	// "sync"
 	"time"
 )
 
 type server struct {
-	ln net.Listener
+	ln   net.Listener
+	exit chan bool
 }
 
-func NewServer() *server {
-	return &server{}
+func NewServer(exit chan bool) *server {
+	return &server{exit: exit}
 }
 
 func (sv *server) StartServer() {
@@ -36,31 +39,38 @@ func (sv *server) AcceptLoop() {
 		fmt.Printf("client connected at: %v\n", conn.RemoteAddr().String())
 		buffer := fmt.Appendf(nil, "connected to %v\n", conn.LocalAddr().String())
 		conn.Write(buffer)
-		go sv.HandleConn(conn)
+		var wg sync.WaitGroup
+		start := time.Now()
+		wg.Add(1)
+		go sv.HandleConn(conn, &wg)
+		wg.Wait()
+		fmt.Printf("handle connection finished in %v\n", time.Since(start))
 	}
 }
 
-func (sv *server) HandleConn(conn net.Conn) {
+func (sv *server) HandleConn(conn net.Conn, wg *sync.WaitGroup) {
 	defer conn.Close()
+	defer wg.Done()
 	sv.ReadLoop(conn)
 }
 
 func (sv *server) ReadLoop(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	jobs := make(chan string)
-	workers := 5
+	workers := 10
 	sv.fanOut(jobs, workers)
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 	for {
 		line, _, err := reader.ReadLine()
 		if err != nil {
-			log.Fatalf("client %v disconnected %v\n", conn.RemoteAddr().String(), err) // so program exit after the client leaves, to track time
+			log.Printf("client %v disconnected %v\n", conn.RemoteAddr().String(), err) // so program exit after the client leaves, to track time
+			sv.exit <- true
 			return
 		}
-		wg.Go(func() {
-			jobs <- string(line)
-		})
-		wg.Wait()
+		// wg.Go(func() {
+		jobs <- string(line)
+		// })
+		// wg.Wait()
 	}
 }
 
@@ -68,6 +78,7 @@ func (sv *server) fanOut(jobs <-chan string, workers int) {
 	for worker := range workers {
 		go func(worker int) {
 			for job := range jobs {
+				// start := time.Now()
 				fmt.Printf("[%v] job \"%v\" by the worker %v\n", time.Now(), job, worker)
 				time.Sleep(3 * time.Second) // simulate processing
 			}
