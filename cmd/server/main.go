1612ef7058f4f1a3d0bdf8a6fceb9a3cf8338587
chetanuchiha16 <chetan16ck@gmail.com>
2026-06-22 17:20:31 +0530

    refactor: clean up imports and restore print statement in main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"metroflow/internal/server"
-	// "os"
-
-	// "os"
-	// "time"
+	"time"
 )
 
 func main() {
-	
-	// fmt.Printf("all jobs finished in %v\n", time.Since(start)) // does this really mean all jobs ? or the handle connection one ?
+	start := time.Now()
 	server := server.NewServer()
 	server.StartServer()
+	fmt.Printf("all jobs finished in %v\n", time.Since(start)) // does this really mean all jobs ? or the handle connection one ?
 	
 
 }
