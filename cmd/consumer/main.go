0376c79a75664e44291f1fddaed113d401345e2c
chetanuchiha16 <chetan16ck@gmail.com>
2026-06-25 20:22:24 +0530

    feat: implement CSV logging for error levels and add log count file

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"metroflow/pkg"
+	"os"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -15,6 +18,14 @@ func main() {
 	})
 	fmt.Println("waiting for job...")
 	mp := make(map[string]int)
+	file, err := os.Create("log_count.csv")
+	if err != nil {
+		fmt.Printf("error opening the csv file error: %v\n", err)
+	}
+
+	csvWriter := csv.NewWriter(file)
+	csvWriter.Write([]string{"Time stamp", "ERROR", "WARN", "INFO"})
+	
 	for {
 		job, err := rdb.BLPop(context.Background(), 0, "job").Result()
 		if err != nil {
@@ -27,6 +38,14 @@ func main() {
 			fmt.Printf("error unmarshel %v", err)
 		}
 		mp[jsonLog.Level] += 1
+		if mp["ERROR"] > 1 {
+			go func() {
+				defer csvWriter.Flush()
+				row := []string{time.Now().Format(time.TimeOnly), fmt.Sprint(mp["ERROR"]), fmt.Sprint(mp["WARN"]), fmt.Sprint(mp["INFO"])}
+				csvWriter.Write(row)
+
+			}()
+		}
 		fmt.Println(mp)
 	}
 }
